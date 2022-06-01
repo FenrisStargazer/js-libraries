24d66f5c5c2220c3d61948374f734eb03739dae5
FenrisStargazer <80987915+FenrisStargazer@users.noreply.github.com>
2022-05-31 19:44:08 -0700

    step1
    used codepen export to get js file working

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-//My javascript stuff
-console.log("Yay it kinda works");
+let circs = ['c1','c2'];
+for (let i = 0; i < circs.length; i++) {
+  new CircleType(document.getElementById(circs[i]))
+   .radius();
+}
+  
+var wiggle = function() {
+  
+}
